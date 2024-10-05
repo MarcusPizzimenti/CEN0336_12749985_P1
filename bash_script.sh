8c73436820cde5f27269a7b05a7d390e4ee9375d
MarcusTrabalho <pizzimentimarcus@usp.br>
2024-10-04 21:05:11 -0300

    Exercício 2 P1

--- bash_script.sh
+++ bash_script.sh
@@ -9,4 +9,4 @@ echo "Gerei uma lista com detalhes da pasta HOME e redireciona para o arquivo li
 ls -l $HOME > lista_HOME.txt # 'ls' lista o conteúdo do diretório em questão. '-l' fornce detalhes adicionais sobre cada arquivo e pasta (permissões, número de links, proprietário, tamanho e data da última modificação). '$HOME' trata do caminho do diretório HOME. '>' indica que a saída do comando deve ser enviada para o arquivo 'lista_HOME.txt'. Esse arquivo de texto será o local para a listagem.
 
 echo "Data atual:"
-date
+date  # Comando que imprime a data atual na tela
